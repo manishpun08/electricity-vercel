5b38f8a4e86b27c0dfe0973d8372eaf6eb51dfa9
manishpun08 <manishpunmagar6@gmail.com>
2025-06-03 14:51:12 +0545

    Add sharp package support and update configuration for image handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["sharp"],
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: "http",
